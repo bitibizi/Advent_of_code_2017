c35a71b1c7ab785e00f7bf1c509a884126a51e83
bitibizi <hanbit199@gmail.com>
2025-11-18 20:50:53 +0100

    finish day1 part 2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -13,9 +13,24 @@ for (var i = 0; i < fileContents.Length; i++)
     if (number == secondNumber)
     {
         sum += int.Parse(number.ToString());
-
-
     }
 }
 
-Console.WriteLine(sum);
+Console.WriteLine("star 1 : "+ sum);
+
+sum=0;
+var halfLength = fileContents.Length / 2;
+
+for (var i = 0; i < fileContents.Length; i++)
+{
+    
+    var number = fileContents[i];
+    var secondNumber = fileContents[(i+halfLength)%fileContents.Length];
+    
+    if (number == secondNumber)
+    {
+        sum += int.Parse(number.ToString());
+    }
+}
+
+Console.WriteLine("star 2 : "+ sum);
