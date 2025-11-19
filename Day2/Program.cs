dedff007a302482fddb36da52e8201ca447a2db1
bitibizi <hanbit199@gmail.com>
2025-11-19 20:38:08 +0100

    finish day2 part 1 task

---
+++ Day2/Program.cs
@@ -0,0 +1,32 @@
+﻿List<List<int>> GetInput()
+{
+    const string filePath = "input.txt";
+    var fileContents = File.ReadLines(filePath).ToList();
+    var input=new List<List<int>>();
+    foreach (var line in fileContents)
+    {
+        var numbers=line.Split('\t').Select(int.Parse).ToList();
+        input.Add(numbers);
+    }
+    
+    return input;
+}
+
+
+void SolvePart1(List<List<int>> input)
+{
+    var sum = 0;
+    foreach (var number in input)
+    {
+        var max = number.Max();
+        var min = number.Min();
+        var diff=max-min;
+        sum += diff;
+        
+    }
+    Console.Write("Part1 sum is "+sum);
+
+}
+
+var input=GetInput();
+SolvePart1(input);
