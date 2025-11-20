285d23ae3012a4b42e8cdb7be0702f5ee4f2a28e
bitibizi <hanbit199@gmail.com>
2025-11-20 22:00:34 +0100

    fisini day 3 part 1

---
+++ Day3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+void Part1(int input)
+{
+    
+    var startNum = Math.Ceiling(Math.Sqrt(input));
+    if (startNum % 2 == 0)
+    {
+        startNum++;
+    }
+
+    var width = startNum - 1;
+    var halfRadius=width / 2;
+    var bottomRightCornerNumber = Math.Pow(startNum, 2);
+    var diff = bottomRightCornerNumber - input;
+    var ganger = Math.Floor(diff / width);
+    var nearestCorner = bottomRightCornerNumber - width * ganger;
+    var middlePoint = nearestCorner - halfRadius;
+    var diffMidllePointAndInput = Math.Abs(middlePoint - input);
+
+    var sum = halfRadius + diffMidllePointAndInput;
+    
+    Console.WriteLine(sum);
+
+}
+var input = 347991;
+Part1(input);
