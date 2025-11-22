4381472efdd6ec9d0b07b2bba4215dc8d99a0165
bitibizi <hanbit199@gmail.com>
2025-11-22 11:56:41 +0100

    finish day 4 part 1

---
+++ Day4/Program.cs
@@ -0,0 +1,33 @@
+﻿List<List<String>> GetInput()
+{
+    const string filePath = "input.txt";
+    var fileContents = File.ReadLines(filePath).ToList();
+    var input=new List<List<String>>();
+    foreach (var line in fileContents)
+    {
+        var words=line.Split(' ').ToList();
+        input.Add(words);
+    }
+    
+    return input;
+}
+
+void Part1(List<List<String>> input)
+{
+    var count = 0;
+    foreach (var line in input)
+    {
+        var lengthList = line.Count;
+        var hashset = new HashSet<string>(line); 
+        var hashsetSize = hashset.Count;
+
+        if (lengthList == hashsetSize)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count);
+}
+
+var input=GetInput();
+Part1(input);
