1b2649efd4fef603fd3b5572e3dbf9afb05c6ac6
bitibizi <hanbit199@gmail.com>
2025-11-24 19:55:54 +0100

    finish day 5 part 1

---
+++ Day5/Program.cs
@@ -0,0 +1,27 @@
+﻿List<int> GetInput()
+{
+    const string filePath = "input.txt";
+    var fileContents = File.ReadLines(filePath).Select(int.Parse).ToList();
+    return fileContents;
+}
+
+void Part1(List<int> input)
+{
+    var position = 0;
+    var step = 0;
+ 
+    while (position < input.Count)
+    { 
+        var oldValue = input[position]; 
+        input[position] ++; 
+        position += oldValue;
+        step++;
+
+    }
+    
+    Console.WriteLine(step);
+
+}
+
+var input = GetInput();
+Part1(input);
