61fbeb7c8406172bf50c55b6e83c76e1d13a03b9
bitibizi <hanbit199@gmail.com>
2025-11-28 12:48:22 +0100

    finish day 7 part1

---
+++ Day7/Program.cs
@@ -0,0 +1,34 @@
+﻿List<string> GetInput()
+{
+    const string filePath = "input.txt";
+    var fileContents = File.ReadLines(filePath).ToList();
+    return fileContents;
+}
+
+void Part1(List<string> input)
+{
+    var numbers = new Dictionary<string, int>();
+    
+    foreach (var i in input)
+    {
+        var word = i.Split(' ')[0];
+        
+        numbers.Add(word, 0);
+        
+        foreach (var j in input)
+        {
+            if (!j.Contains(word)) continue;
+            numbers[word]++;
+            
+        }
+    }
+
+    if (numbers.ContainsValue(1))
+    {
+        var result = numbers.FirstOrDefault(x => x.Value == 1);
+        Console.WriteLine(result);
+    }
+}
+
+var input = GetInput();
+Part1(input);
